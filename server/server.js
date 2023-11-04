4ff8823bf50faa58069c2e475fa52c92698f90b2
Lester Jerome <ortizlester777@gmail.com>
2023-11-04 21:51:52 +0800

    added inventory

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = 3001; // Choose a port for your server
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root', // Replace with your MySQL username
-  password: 'admin123', // Replace with your MySQL password
+  password: 'admin', // Replace with your MySQL password
   database: '11dcommercedb'
 });
 
@@ -28,63 +28,76 @@ app.use(cors());
 
 // Register endpoint
 app.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
-  
-    try {
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-      // Insert user into database with hashed password
-      const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-      db.query(query, [name, email, hashedPassword], (err, result) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Internal Server Error');
-        } else {
-          console.log('User registered');
-          res.send('User registered');
-        }
-      });
-    } catch (error) {
-      console.error('Error hashing password:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+  const { name, email, password } = req.body;
+
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Insert user into database with hashed password
+    const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+    db.query(query, [name, email, hashedPassword], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        console.log('User registered');
+        res.send('User registered');
+      }
+    });
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 // Login endpoint
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-  
-    // Check if the email and password are provided
-    if (!email || !password) {
-      return res.status(400).json({ message: 'Email and password are required' });
-    }
-  
-    // Query the database to check if the user exists
-    db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
-      if (err) {
-        console.error('Error querying the database:', err);
-        return res.status(500).json({ message: 'Internal server error' });
-      }
-  
-      if (results.length === 0) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      const user = results[0];
-  
-      // Check if the password is correct
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      
+  const { email, password } = req.body;
 
-      if (!isPasswordValid) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // If both email and password are valid, you can send a success response
-      res.json({ message: 'Login successful', user: { id: user.id, name: user.name, email: user.email } });
-    });
+  // Check if the email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  // Query the database to check if the user exists
+  db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const user = results[0];
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // If both email and password are valid, you can send a success response
+    res.json({ message: 'Login successful', user: { id: user.id, name: user.name, email: user.email } });
   });
+});
+
+// Inventory endpoint
+app.get('/api/inventory', (req, res) => {
+  // Query the database to retrieve inventory data
+  const query = 'SELECT * FROM inventory';
+  db.query(query, (error, results) => {
+    if (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import ProductsPage from "./components/product-catalog/productsPage";
+import Inventory from "./components/inventory/inventory";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<ProductsPage />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/inventory" element={<Inventory />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
