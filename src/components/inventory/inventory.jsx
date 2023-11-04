bda50727be2aaf00da9cd60c92015e2ce4977ff3
Lester Jerome <ortizlester777@gmail.com>
2023-11-04 21:52:24 +0800

    changed color

--- src/components/inventory/inventory.jsx
+++ src/components/inventory/inventory.jsx
@@ -29,7 +29,7 @@ export default function Inventory() {
         <div className="mt-3 md:mt-0">
           <a
             href="javascript:void(0)"
-            className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
+            className="inline-block px-4 py-2 text-white duration-150 font-medium bg-rose-600 rounded-lg hover:bg-rose-500 active:bg-rose-700 md:text-sm"
           >
             Add product
           </a>
