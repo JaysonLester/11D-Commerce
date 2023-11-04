0538d345e47d847c86746b69da06affaaf5ced21
Lester Jerome <ortizlester777@gmail.com>
2023-11-04 17:55:15 +0800

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
+import ProductsPage from "./components/product-catalog/productsPage";
 
 function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element="{}"></Route>
+          <Route path="/" element={<ProductsPage />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
         </Routes>
---
+++ src/components/product-catalog/productsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../navigation-bar/nav'
+
+function ProductsPage() {
+    return (
+        <div>
+            <Navbar />
+            
+        </div>
+    )
+}
+
+export default ProductsPage
