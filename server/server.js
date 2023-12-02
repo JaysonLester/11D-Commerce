3ace2b3133da6d53c0501367c5f215cc3be604e3
Lester Jerome <142711594+mewoJiwo@users.noreply.github.com>
2023-12-02 15:20:15 +0800

    hashed confirm password

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ app.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert user into the database with the hashed password
-    const query = 'INSERT INTO users (name, email, password, confirm_password) VALUES (?, ?, ?, ?)';
-    db.query(query, [name, email, hashedPassword, confirmPassword], (err, result) => {
+    const query = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+    db.query(query, [name, email, hashedPassword], (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
@@ -62,23 +62,17 @@ app.post('/register', async (req, res) => {
   }
 });
 
-
 // Register endpoint for admin users
 app.post('/register/admin', async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body;
+  const { name, email, password } = req.body;
 
   try {
-    // Validate the confirmPassword field
-    if (password !== confirmPassword) {
-      return res.status(400).send('Passwords do not match');
-    }
-
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert admin user into the database with the hashed password
-    const query = 'INSERT INTO users (name, email, password, confirm_password, admin) VALUES (?, ?, ?, ?, ?)';
-    db.query(query, [name, email, hashedPassword, confirmPassword, 1], (err, result) => {
+    const query = 'INSERT INTO users (name, email, password, admin) VALUES (?, ?, ?, ?)';
+    db.query(query, [name, email, hashedPassword, 1], (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
@@ -92,6 +86,7 @@ app.post('/register/admin', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
 
 // Login endpoint
 app.post('/login', async (req, res) => {
