adf586725ca529e5c516960492626feb26732aef
Lester Jerome <142711594+mewoJiwo@users.noreply.github.com>
2023-12-11 17:33:32 +0800

    updated generate report modal

--- src/components/orders/modals/GenerateReportModal.jsx
+++ src/components/orders/modals/GenerateReportModal.jsx
@@ -1,60 +1,59 @@
-import React, { useState } from 'react';
+    // GenerateReportModal.js
+    import React from 'react';
+    import { Dialog, DialogTitle, DialogContent, TextField, Button, Grid, createTheme, ThemeProvider } from '@mui/material';
+    import { useFormik } from 'formik';
+    import { DatePicker } from '@mui/lab';
+    import AdapterDateFns from '@mui/lab/AdapterDateFns';
+    import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
-const GenerateReportModal = ({ onClose, onGenerateReport }) => {
-  const [reportData, setReportData] = useState({
-    date: '',
-    status: '',
-    productType: '',
-    color: '',
-    size: '',
-    productCode: '',
-  });
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#000000', // Black for primary color
+            },
+            secondary: {
+                main: '#808080', // Gray for secondary color
+            },
+        },
+    });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setReportData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+    const GenerateReportModal = ({ onClose, onGenerateReport }) => {
+        const formik = useFormik({
+            initialValues: {
+                date: null,
+                status: '',
+                productType: '',
+                color: '',
+                size: '',
+                productCode: '',
+            },
+            onSubmit: (values) => {
+                onGenerateReport(values);
+                onClose();
+            },
+        });
 
-  const handleGenerateReport = () => {
-    onGenerateReport(reportData);
-    onClose();
-  };
+        return (
+            <ThemeProvider theme={theme}>
+                <Dialog open={true} onClose={onClose}>
+                    <DialogTitle className="text-center">Generate Report?</DialogTitle>
+                    <DialogContent>
+                        <form onSubmit={formik.handleSubmit}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <Button type="submit" variant="contained" color="primary">
+                                        Generate
+                                    </Button>
+                                    <Button onClick={onClose} variant="outlined" color="secondary">
+                                        Close
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    </DialogContent>
+                </Dialog>
+            </ThemeProvider>
+        );
+    };
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-      <div className="bg-white p-8 max-w-md rounded shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Generate Report</h2>
-
-        {/* Date Input */}
-        <div className="mb-4">
-         
-          <input
-            value={reportData.date}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        {/* Other Input Fields (Status, Product Type, Color, Size, Product Code) */}
-        {/* ... (Add similar structure for other input fields) */}
-
-        {/* Buttons (Generate Report and Close) */}
-        <div className="flex justify-end">
-          <button
-            onClick={handleGenerateReport}
-            className="mr-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          >
-            Generate Report
-          </button>
-          <button onClick={onClose} className="px-4 py-2 border border-gray-300 rounded">
-            Close
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default GenerateReportModal;
+    export default GenerateReportModal;
