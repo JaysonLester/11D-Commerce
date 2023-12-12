8798b22778c2b02112bc364177466551ec693874
Lester Jerome <142711594+mewoJiwo@users.noreply.github.com>
2023-12-13 01:39:00 +0800

    updated changePassword field

--- src/components/change-password/changePassword.jsx
+++ src/components/change-password/changePassword.jsx
@@ -3,8 +3,11 @@ import Nav from '../navigation-bar/nav';
 import axios from 'axios';
 import bcrypt from 'bcryptjs';
 import _debounce from 'lodash/debounce';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 export default function ChangePassword() {
   const tokenEncoded = localStorage.getItem('token');
@@ -190,90 +193,96 @@ export default function ChangePassword() {
           <div className="pb-8 sm:pb-12">
             <h2 className="text-lg font-semibold text-gray-900">Security</h2>
             <p className="mt-1 text-sm text-gray-600">Update your password</p>
-  
+
             {/* Old Password */}
             <div className="mt-4 sm:mt-6 relative">
-              <label htmlFor="old-password" className="block text-sm font-semibold text-gray-900">
-                Old Password
-              </label>
-              <div className="mt-2 relative">
-                <input
-                  type={showOldPassword ? 'text' : 'password'}
-                  name="old-password"
-                  id="old-password"
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-2.5 px-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-zinc-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => setOldPasswordWithValidation(e.target.value)}
-                />
-                <div
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer"
-                  onClick={() => setShowOldPassword(!showOldPassword)}
-                >
-                  <FontAwesomeIcon icon={showOldPassword ? faEye : faEyeSlash} />
-                </div>
-              </div>
-              {isOldPasswordCorrect || oldPassword.trim() === '' ? null : (
-                <p className="mt-2 text-sm text-red-500">Incorrect old password</p>
-              )}
+              <TextField
+                label="Old Password"
+                type={showOldPassword ? 'text' : 'password'}
+                required
+                autoComplete="current-password"
+                onChange={(e) => setOldPasswordWithValidation(e.target.value)}
+                error={!isOldPasswordCorrect && oldPassword.trim() !== ''}
+                helperText={!isOldPasswordCorrect && oldPassword.trim() !== '' ? 'Incorrect old password' : ''}
+                variant="outlined"
+                fullWidth
+                sx={{ mt: 2 }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowOldPassword(!showOldPassword)}
+                      >
+                        {showOldPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
-  
+
             {/* New Password */}
             <div className={`mt-4 sm:mt-6 ${isOldPasswordCorrect ? '' : 'hidden'}`}>
-              <label htmlFor="new-password" className="block text-sm font-semibold text-gray-900">
-                New Password
-              </label>
-              <div className="mt-2 relative">
-                <input
-                  type={showNewPassword ? 'text' : 'password'}
-                  name="new-password"
-                  id="new-password"
-                  autoComplete="new-password"
-                  className={`block w-full rounded-md border-0 py-2.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-zinc-600 sm:text-sm sm:leading-6 ${passwordError && 'border-red-500'}`}
-                  onChange={(e) => setPasswordWithValidation(e.target.value)}
-                />
-                <div
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer"
-                  onClick={() => setShowNewPassword(!showNewPassword)}
-                >
-                  <FontAwesomeIcon icon={showNewPassword ? faEye : faEyeSlash} />
-                </div>
-              </div>
-              {passwordError && (
-                <p className="mt-2 text-sm text-red-500">{passwordError}</p>
-              )}
+              <TextField
+                label="New Password"
+                type={showNewPassword ? 'text' : 'password'}
+                required
+                autoComplete="new-password"
+                onChange={(e) => setPasswordWithValidation(e.target.value)}
+                error={!!passwordError}
+                helperText={passwordError}
+                variant="outlined"
+                fullWidth
+                sx={{ mt: 2 }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowNewPassword(!showNewPassword)}
+                      >
+                        {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
-  
+
             {/* Confirm Password */}
             <div className={`mt-4 sm:mt-6 ${isOldPasswordCorrect ? '' : 'hidden'}`}>
-              <label htmlFor="confirm-new-password" className="block text-sm font-semibold text-gray-900">
-                Confirm New Password
-              </label>
-              <div className="mt-2 relative">
-                <input
-                  type={showConfirmNewPassword ? 'text' : 'password'}
-                  name="confirm-new-password"
-                  id="confirm-new-password"
-                  autoComplete="new-password"
-                  className={`block w-full rounded-md border-0 py-2.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-zinc-600 sm:text-sm sm:leading-6 ${confirmPasswordError && 'border-red-500'}`}
-                  value={confirmNewPassword}
-                  onChange={(e) => setConfirmPasswordWithValidation(e.target.value)}
-                />
-                <div
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-400 cursor-pointer"
-                  onClick={() => setShowConfirmNewPassword(!showConfirmNewPassword)}
-                >
-                  <FontAwesomeIcon icon={showConfirmNewPassword ? faEye : faEyeSlash} />
-                </div>
-              </div>
-              {confirmPasswordError && (
-                <p className="mt-2 text-sm text-red-500">{confirmPasswordError}</p>
-              )}
+              <TextField
+                label="Confirm New Password"
+                type={showConfirmNewPassword ? 'text' : 'password'}
+                required
+                autoComplete="new-password"
+                value={confirmNewPassword}
+                onChange={(e) => setConfirmPasswordWithValidation(e.target.value)}
+                error={!!confirmPasswordError}
+                helperText={confirmPasswordError}
+                variant="outlined"
+                fullWidth
+                sx={{ mt: 2 }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={() => setShowConfirmNewPassword(!showConfirmNewPassword)}
+                      >
+                        {showConfirmNewPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
             </div>
-  
+
           </div>
-  
+
           {/* Save Button */}
-          <div className="mt-2 sm:mt-4 flex justify-end"> 
+          <div className="mt-0 sm:mt-2 flex justify-end">
             <button
               type="submit"
               className="rounded-md bg-zinc-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-zinc-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-zinc-600"
@@ -285,6 +294,6 @@ export default function ChangePassword() {
       </div>
     </div>
   );
-  
-}  
+
+}
 
