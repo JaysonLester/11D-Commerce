97f7e8094257922dff5cf4c112500871f0bc7220
Lester Jerome <142711594+mewoJiwo@users.noreply.github.com>
2023-12-15 08:18:56 +0800

    fixed admin registration endpoint

--- server/server.js
+++ server/server.js
@@ -124,8 +124,8 @@ app.post('/register/admin', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert admin user into the database with the hashed password
-    const query = 'INSERT INTO users (name, email, password, admin) VALUES (?, ?, ?, ?)';
-    db.query(query, [name, email, hashedPassword, 1], (err, result) => {
+    const query = 'INSERT INTO users (name, email, password, confirm_password, admin) VALUES (?, ?, ?, ?, ?)';
+    db.query(query, [name, email, hashedPassword, confirmPassword, 1], (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
