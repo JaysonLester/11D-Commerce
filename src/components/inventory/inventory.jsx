672edd1fcc635dc96d1301b0e2e79ed259b26985
Lester Jerome <142711594+mewoJiwo@users.noreply.github.com>
2023-12-19 13:30:29 +0800

    Update inventory.jsx

--- src/components/inventory/inventory.jsx
+++ src/components/inventory/inventory.jsx
@@ -98,6 +98,7 @@ export default function Inventory() {
   };
 
   const handleOpenUpdateModal = (item) => {
+    console.log(item);
     setItemToUpdate(item);
     setIsUpdateModalOpen(true);
   };
